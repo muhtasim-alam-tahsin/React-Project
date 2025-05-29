b83d1d3ac2ed18b3daf4f649e1d8bad33981599d
muhtasim-alam-tahsin <muhtasimtahsin258.a@gmail.com>
2025-05-29 07:04:18 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.jsx'
 import Weather from './Weather_App/WeatherApp'
 import './Weather_App/Weather.css';
 
